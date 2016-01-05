9e7836716569d214b37933600d14a39f7ad3833d
Sam Groesch <samgroesch@gmail.com>
2016-01-05 13:07:36 -0600

    Fixed the database

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
+require('./db/database');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
