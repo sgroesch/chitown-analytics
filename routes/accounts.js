7dc521713d19590bcb72743632f5bb8742d2a3e7
Sam Groesch <samgroesch@gmail.com>
2016-01-07 15:57:31 -0600

    Adding some length verification

--- routes/accounts.js
+++ routes/accounts.js
@@ -19,22 +19,42 @@ router.post('/login', passport.authenticate('local', { failureRedirect: '/' }),
 );
 
 router.post('/register', function(req, res){
+  var checkUsername = checkLength(req.body.username, 16);
+  var checkEmail = checkLength(req.body.email, 50);
+  var checkPassword = checkLength(req.body.password, 16);
+  // if (checkUsername == false || checkEmail == false || checkPassword == false) {
+  //   console.log('Worked');
+  //   res.redirect('/');
+  // }
   Models.Account.register(new Models.Account({
-    username: req.body.username,
-    email: req.body.email
-  }),
-  req.body.password,
-  function(err, account) {
-    if (err) {
-      return res.render('/', { account: account });
-      // message to require registering
+      username: req.body.username,
+      email: req.body.email
+    }),
+    req.body.password,
+    function(err, account) {
+      if (err) {
+        return res.render('/', { account: account });
+        // message to require registering
+      }
+      passport.authenticate('local')(req, res, function() {
+        res.redirect('/');
+      });
     }
-    passport.authenticate('local')(req, res, function() {
-      res.redirect('/');
-    });
-  }
   );
-})
+});
+
+var checkLength = function(inputToCheck, maxLength) {
+  if (inputToCheck.length < 5 || inputToCheck.length > maxLength) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+var checkForEmail = function() {
+  // Checks if email is registered already
+};
+
 
 router.get('/logout', function(req, res){
   req.logout();
